755b1fd4a6012e7d7d59ea0a2020b4ccd627ba79
Jakub Swierczek <jakub.swierczek@outlook.com>
2023-05-18 10:31:09 +0200

    update readme

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import process from "process";
-import * as dotenv from 'dotenv'
+import * as dotenv from "dotenv";
 
-
-dotenv.config({ path: `${process.cwd()}/.env`})
+dotenv.config({ path: `${process.cwd()}/.env` });
 
 const blockNumber = process.env.BLOCK_NUMBER!;
-const chainId = process.env.CHAIN_ID ?? '2137';
+const chainId = process.env.CHAIN_ID ?? "2137";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     local: {
-      url: 'http://127.0.0.1:8545',
+      url: "http://127.0.0.1:8545",
       timeout: 1000000,
       chainId: Number(chainId),
     },
@@ -26,12 +25,12 @@ const config: HardhatUserConfig = {
       mining: {
         auto: true,
       },
-      hardfork: 'london',
-      gas: 'auto',
+      hardfork: "london",
+      gas: "auto",
       initialBaseFeePerGas: 1000000000,
       allowUnlimitedContractSize: true,
     },
-  }
+  },
 };
 
 export default config;
--- update-block-number.ts
+++ update-block-number.ts
@@ -1,52 +1,49 @@
-import fetch from 'node-fetch'
-import { existsSync, writeFileSync, readFileSync } from 'fs';
+import fetch from "node-fetch";
+import { existsSync, writeFileSync, readFileSync } from "fs";
 import process from "process";
-import {ethers} from "ethers";
+import { ethers } from "ethers";
 
-const envFile = `${process.cwd()}/.env`
-const newLine = '\n'
-const blockNumberOffset = 10
+const envFile = `${process.cwd()}/.env`;
+const newLine = "\n";
+const blockNumberOffset = 10;
 
 const updateNumber = async () => {
-    const rpcUrl = process.env.MAINNET_URL!
+  const rpcUrl = process.env.MAINNET_URL!;
 
-    const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
-    const currentBlockNumber = await provider.getBlockNumber()
-    const forkingBlockNumber = currentBlockNumber - blockNumberOffset
-    if (!currentBlockNumber) {
-        console.error('❌ Block height not found')
-        return
-    } else {
-        console.log('😊 Found block height:', currentBlockNumber)
-    }
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+  const currentBlockNumber = await provider.getBlockNumber();
+  const forkingBlockNumber = currentBlockNumber - blockNumberOffset;
+  if (!currentBlockNumber) {
+    console.error("❌ Block height not found");
+    return;
+  } else {
+    console.log("😊 Found block height:", currentBlockNumber);
+  }
 
-    process.env.BLOCK_NUMBER = `${forkingBlockNumber}`
+  process.env.BLOCK_NUMBER = `${forkingBlockNumber}`;
 
-    console.log(`🙌 Updated the block number with ${forkingBlockNumber}`)
+  console.log(`🙌 Updated the block number with ${forkingBlockNumber}`);
 
-    if (!existsSync(envFile)) {
-        console.log(`✨ Creating .env file at ${envFile}`)
-        writeFileSync(envFile, '', { flag: 'w' })
-    }
+  if (!existsSync(envFile)) {
+    console.log(`✨ Creating .env file at ${envFile}`);
+    writeFileSync(envFile, "", { flag: "w" });
+  }
 
+  const envFileContents = readFileSync(envFile, "utf8");
+  const envFileContentLines = envFileContents
+    .split(newLine)
+    .filter((line: string) => !line.startsWith("BLOCK_NUMBER"));
+  envFileContentLines.push(`BLOCK_NUMBER=${forkingBlockNumber}`);
 
-    const envFileContents = readFileSync(envFile, 'utf8')
-    const envFileContentLines = envFileContents
-        .split(newLine)
-        .filter((line: string) => !line.startsWith('BLOCK_NUMBER'))
-    envFileContentLines.push(`BLOCK_NUMBER=${forkingBlockNumber}`)
+  writeFileSync(envFile, envFileContentLines.join(newLine));
 
-    writeFileSync(envFile, envFileContentLines.join(newLine))
+  try {
+    writeFileSync(envFile, envFileContentLines.join(newLine));
 
-
-    try {
-        writeFileSync(envFile, envFileContentLines.join(newLine))
-
-        console.log(`🙌 Updated the block number with ${forkingBlockNumber}`)
-
-    } catch (error) {
-        console.error(`❌ Could not update the block number (${error})`)
-    }
-    console.log('')
-}
-updateNumber()
+    console.log(`🙌 Updated the block number with ${forkingBlockNumber}`);
+  } catch (error) {
+    console.error(`❌ Could not update the block number (${error})`);
+  }
+  console.log("");
+};
+updateNumber();
